f606129e02323991c35437bdc0ad7f9332f006bf
Rangga <rangga.bagas0@gmail.com>
2019-08-20 17:38:22 +0700

    create basic UI

--- app/src/main/java/com/newtonapp/view/ui/VerificationActivity.java
+++ app/src/main/java/com/newtonapp/view/ui/VerificationActivity.java
@@ -1,14 +1,16 @@
-package com.newtonapp;
+package com.newtonapp.view.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.newtonapp.R;
+
+public class VerificationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_verification);
     }
 }
