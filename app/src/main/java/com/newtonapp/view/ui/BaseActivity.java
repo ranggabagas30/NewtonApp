5275ee3fc9d221c89a55e76b6e75b8e0c666a2af
Rangga <rangga.bagas0@gmail.com>
2019-08-27 13:35:28 +0700

    done basic ui + flow

---
+++ app/src/main/java/com/newtonapp/view/ui/BaseActivity.java
@@ -0,0 +1,26 @@
+package com.newtonapp.view.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    protected void navigateTo(Activity activity, Class targetActivity) {
+        Intent intent = new Intent(activity, targetActivity);
+        startActivity(intent);
+    }
+
+    protected void navigateTo(Activity activity, Class targetActivity, int mode) {
+        Intent intent = new Intent(activity, targetActivity);
+        intent.setFlags(mode);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/newtonapp/view/ui/MainActivity.java
+++ app/src/main/java/com/newtonapp/view/ui/MainActivity.java
@@ -1,16 +1,27 @@
 package com.newtonapp.view.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.newtonapp.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+
+    private AppCompatButton btnConfirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnConfirm = findViewById(R.id.main_btn_confirm);
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navigateTo(MainActivity.this, VerificationActivity.class);
+            }
+        });
     }
 }
