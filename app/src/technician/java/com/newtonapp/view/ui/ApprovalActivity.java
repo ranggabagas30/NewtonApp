ce1b665d4bd6d856bee5ed57b07186dc45806d36
Rangga <rangga.bagas0@gmail.com>
2019-09-10 22:38:24 +0700

    add approval, product history, report, and report detail activity

---
+++ app/src/technician/java/com/newtonapp/view/ui/ApprovalActivity.java
@@ -0,0 +1,16 @@
+package com.newtonapp.view.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.newtonapp.R;
+
+public class ApprovalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_approval);
+    }
+}
---
+++ app/src/technician/java/com/newtonapp/view/ui/ProductHistoryActivity.java
@@ -0,0 +1,16 @@
+package com.newtonapp.view.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.newtonapp.R;
+
+public class ProductHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_history);
+    }
+}
