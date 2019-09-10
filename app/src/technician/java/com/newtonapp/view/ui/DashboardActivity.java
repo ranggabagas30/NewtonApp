57e3b46e49ba7265a3adab51ee8bbf9ac27ebb51
Rangga <rangga.bagas0@gmail.com>
2019-09-10 14:34:03 +0700

    add svg support

--- app/src/technician/java/com/newtonapp/view/ui/DashboardActivity.java
+++ app/src/technician/java/com/newtonapp/view/ui/DashboardActivity.java
@@ -1,16 +1,26 @@
 package com.newtonapp.view.ui;
 
+import android.os.Bundle;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.newtonapp.R;
+import com.pixplicity.sharp.Sharp;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    private GridLayout glLayoutDashboardBody;
+    private LinearLayout llLayoutDashboardMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        //glLayoutDashboardBody = findViewById(R.id.dashboard_layout_body);
+        llLayoutDashboardMenu = findViewById(R.id.dashboard_layout_menu);
+
+        Sharp.loadResource(getResources(), R.raw.logo_portrait).into(llLayoutDashboardMenu);
     }
 }
