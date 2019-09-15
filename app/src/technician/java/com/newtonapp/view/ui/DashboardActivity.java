96d60529b55554519ed62163707c651442723c9e
Rangga <rangga.bagas0@gmail.com>
2019-09-15 19:44:45 +0700

    add action to outstanding and solving button

--- app/src/technician/java/com/newtonapp/view/ui/DashboardActivity.java
+++ app/src/technician/java/com/newtonapp/view/ui/DashboardActivity.java
@@ -4,23 +4,27 @@ import android.os.Bundle;
 import android.widget.GridLayout;
 import android.widget.LinearLayout;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import com.newtonapp.R;
-import com.pixplicity.sharp.Sharp;
 
-public class DashboardActivity extends AppCompatActivity {
+public class DashboardActivity extends BaseActivity {
 
     private GridLayout glLayoutDashboardBody;
     private LinearLayout llLayoutDashboardMenu;
+    private CardView cvOutstanding, cvSolving, cvReport;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        //glLayoutDashboardBody = findViewById(R.id.dashboard_layout_body);
+        glLayoutDashboardBody = findViewById(R.id.dashboard_layout_body);
         llLayoutDashboardMenu = findViewById(R.id.dashboard_layout_menu);
+        cvOutstanding         = findViewById(R.id.dashboard_cv_outstanding);
+        cvSolving             = findViewById(R.id.dashboard_cv_solving);
+        cvReport              = findViewById(R.id.dashboard_cv_report);
 
-        Sharp.loadResource(getResources(), R.raw.logo_portrait).into(llLayoutDashboardMenu);
+        cvOutstanding.setOnClickListener(view -> navigateTo(this, OutstandingActivity.class));
+        cvSolving.setOnClickListener(view -> navigateTo(this, SolvingActivity.class));
     }
 }
