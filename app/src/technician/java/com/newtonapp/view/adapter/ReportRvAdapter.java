bb32ab65441d86012ae913af4bd4a847d1a64008
Rangga <rangga.bagas0@gmail.com>
2019-09-17 09:50:46 +0700

    add report recyclerview adapter

---
+++ app/src/technician/java/com/newtonapp/view/adapter/ReportRvAdapter.java
@@ -0,0 +1,99 @@
+package com.newtonapp.view.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.newtonapp.R;
+import com.newtonapp.model.ReportRvModel;
+
+import java.util.ArrayList;
+
+public class ReportRvAdapter extends RecyclerView.Adapter<ReportRvAdapter.ViewHolder> {
+
+    private ArrayList<ReportRvModel> reportList;
+    private OnClickListener onClickListener;
+
+    public ReportRvAdapter(ArrayList<ReportRvModel> reportList) {
+        this.reportList = reportList;
+    }
+
+    public void setReportList(ArrayList<ReportRvModel> reportList) {
+        this.reportList = reportList;
+    }
+
+    public void setOnClickListener(OnClickListener onClickListener) {
+        this.onClickListener = onClickListener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View reportItemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_report, parent, false);
+        return new ViewHolder(reportItemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        ReportRvModel report = reportList.get(position);
+        holder.setReport(report);
+        holder.setOnClickListener(onClickListener);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (reportList == null) return 0;
+        else return reportList.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        AppCompatTextView tvReportDate;
+        AppCompatTextView tvReportIDPrinter;
+        AppCompatTextView tvReportIDCustomer;
+        AppCompatTextView tvReportStatus;
+        OnClickListener onClickListener;
+        ReportRvModel report;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvReportDate = itemView.findViewById(R.id.report_tv_date);
+            tvReportIDPrinter = itemView.findViewById(R.id.report_tv_idprinter);
+            tvReportIDCustomer = itemView.findViewById(R.id.report_tv_idcustomer);
+            tvReportStatus = itemView.findViewById(R.id.report_tv_status);
+            itemView.setOnClickListener(view -> onClickListener.onClick(report));
+        }
+
+        private void setReport(ReportRvModel report) {
+            this.report = report;
+            notifyDataSetChanged();
+        }
+
+        private void notifyDataSetChanged() {
+            tvReportDate.setText(report.getIssueddate());
+            tvReportIDPrinter.setText(report.getIdprinter());
+            tvReportIDCustomer.setText(report.getIdcustomer());
+            tvReportStatus.setText(report.getStatus());
+            tvReportStatus.setTextColor(getStatusColor());
+        }
+
+        private void setOnClickListener(OnClickListener onClickListener) {
+            this.onClickListener = onClickListener;
+        }
+
+        private int getStatusColor() {
+            int statusColor = R.color.status_hold;
+            if (report.getStatus().equalsIgnoreCase(itemView.getContext().getString(R.string.status_solved)))
+                statusColor = R.color.status_solved;
+            return itemView.getContext().getResources().getColor(statusColor);
+        }
+    }
+
+    public interface OnClickListener {
+        void onClick(ReportRvModel report);
+    }
+}
