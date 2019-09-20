4d7831df23c1235f98d8577e938b0fbd5e60bb70
Rangga <rangga.bagas0@gmail.com>
2019-09-20 11:16:55 +0700

    add random generator util

--- app/src/main/java/com/newtonapp/utility/CommonUtil.java
+++ app/src/main/java/com/newtonapp/utility/CommonUtil.java
@@ -2,6 +2,8 @@ package com.newtonapp.utility;
 
 import android.text.TextUtils;
 
+import java.util.Random;
+
 public class CommonUtil {
 
     public static boolean isLoginValidated(String username, String password) {
@@ -10,5 +12,11 @@ public class CommonUtil {
             isValid = true;
         }
         return isValid;
+    }
+
+    public static int getRandomIntRange(int min, int max) {
+        if (min >= max) throw new IllegalArgumentException("min should be lower than max");
+
+        return new Random().nextInt((max - min) + 1) + min;
     }
 }
