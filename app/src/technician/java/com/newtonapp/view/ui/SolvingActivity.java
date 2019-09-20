ce5e15ad881f11061a8171341944821a78d55b88
Rangga <rangga.bagas0@gmail.com>
2019-09-20 11:17:38 +0700

    add hold button listener

--- app/src/technician/java/com/newtonapp/view/ui/SolvingActivity.java
+++ app/src/technician/java/com/newtonapp/view/ui/SolvingActivity.java
@@ -1,19 +1,25 @@
 package com.newtonapp.view.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.AppCompatEditText;
 import androidx.appcompat.widget.Toolbar;
 
 import com.newtonapp.R;
+import com.newtonapp.utility.Constants;
 
 public class SolvingActivity extends BaseActivity {
 
+    private static final String TAG = SolvingActivity.class.getSimpleName();
     private Toolbar toolbar;
     private AppCompatEditText etIdCustomer;
     private AppCompatEditText etIdBarcode;
@@ -29,6 +35,31 @@ public class SolvingActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_solving);
+        initView();
+        setListener();
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        supportNavigateUpTo(this, DashboardActivity.class);
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        String barcodeValue = data != null ? data.getStringExtra(Constants.EXTRA_INTENT_RESULT_SCAN) : null;
+
+        Log.d(TAG, "requestCode : " + requestCode);
+        Log.d(TAG, "resultCode : " + resultCode);
+        Log.d(TAG, "data barcode : " + barcodeValue);
+        if (requestCode == Constants.RC_SCAN_BARCODE && resultCode == RESULT_OK) {
+            etIdBarcode.setText(barcodeValue);
+        }
+    }
+
+    private void initView() {
         toolbar = findViewById(R.id.header_layout_toolbar);
         etIdCustomer = findViewById(R.id.solving_et_idcustomer);
         etIdBarcode = findViewById(R.id.solving_et_idbarcode);
@@ -39,7 +70,9 @@ public class SolvingActivity extends BaseActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(R.string.screen_solving);
+    }
 
+    private void setListener() {
         btnSolved.setOnClickListener(view -> solved());
         btnHold.setOnClickListener(view -> hold());
         etIdCustomer.addTextChangedListener(new TextWatcher() {
@@ -74,6 +107,24 @@ public class SolvingActivity extends BaseActivity {
                 idPrinter = editable.toString();
             }
         });
+
+        etIdBarcode.setOnTouchListener((view, event) -> {
+            final int DRAWABLE_LEFT = 0;
+            final int DRAWABLE_TOP = 1;
+            final int DRAWABLE_RIGHT = 2;
+            final int DRAWABLE_BOTTOM = 3;
+
+            if(event.getAction() == MotionEvent.ACTION_UP) {
+                if(event.getRawX() >= (etIdBarcode.getRight() - etIdBarcode.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                    // your action here
+                    Intent openCameraIntent = new Intent(this, CameraPreviewActivity.class);
+                    startActivityForResult(openCameraIntent, Constants.RC_SCAN_BARCODE);
+                    return true;
+                }
+            }
+            return false;
+        });
+
         etNote.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -91,20 +142,17 @@ public class SolvingActivity extends BaseActivity {
             }
         });
     }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        supportNavigateUpTo(this, DashboardActivity.class);
-        return true;
-    }
-
+    
     private void solved() {
+        // TODO: add navigation method with order detail data bundle
         if (isValid()) navigateTo(this, ApprovalActivity.class);
         else Toast.makeText(this, "Please fill the empty fields", Toast.LENGTH_SHORT).show();
     }
 
     private void hold() {
-
+        // TODO: add navigation method with printer's historical usage data bundle
+        if (isValid()) navigateTo(this, ProductHistoryActivity.class);
+        else Toast.makeText(this, "Please fill the empty fields", Toast.LENGTH_SHORT).show();
     }
 
     private boolean isValid() {
